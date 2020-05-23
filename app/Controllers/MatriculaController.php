3ad4dd2f98778433c37c4753a9a0a645ab8bc89b
Fernando Daniel <ferdanymr@gmail.com>
2020-05-23 00:06:29 -0500

    prueba 1

--- app/Controllers/MatriculaController.php
+++ app/Controllers/MatriculaController.php
@@ -6,16 +6,18 @@ use App\Models\{Alumno, Adjunto, Administrador};
 
 class MatriculaController extends BaseController{
 
-   public __construct($usr){
+   public function __construct($usr){
       $this->usr = $usr;
    }
 
    public function AsignarMatricula(){
       //crear matricula de 5 digitos XXXXX
       //ultimo digito para asignar matricula 1.- Alumno 2.-Adjunto 3.-Administrador
-       switch($usr){
+       switch($this->usr){
           case "Alum":{
-             if(/*verificar ultima matricula*/)
+             if(/*verificar ultima matricula*/ true){
+
+             }
           }
           break;
           case "Adju":{
