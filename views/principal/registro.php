27f3d65e5873eb14c01b85a3069a6f1099a57042
jesus alexis cabrera <jesuscabrera14@aragon.unam.mx>
2020-05-27 15:42:19 -0500

    nuevo index y mejorado

--- views/principal/registro.php
+++ views/principal/registro.php
@@ -20,6 +20,7 @@ use App\Controllers\{ConsultaController, RegistrarController};
   <title>Registro</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
 
 </head>
 
@@ -49,165 +50,197 @@ use App\Controllers\{ConsultaController, RegistrarController};
     <h1>Registro Alumno</h1>
   </div>
   <div class="container">
+        <form action="" method="POST">
+            <hr class="solid" style="border-top: 3px solid rgb(134, 8, 8)">
+            <div class="modal-dialog text-center">
+                <h3>Datos Personales</h3>
+            </div>
+            <div class="form-row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <label for="validationServer01">Primer Nombre</label>
+                    <input type="text" class="form-control" id="validationServer01" required name="firstName">
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+                <div class="invalid-feedback">More example invalid feedback text</div>
+                <div class="valid-feedback">ok</div>
+                <div class="col-sm-4">
+                    <label for="validationServer02">Segundo Nombre (Opcional)</label>
+                    <input type="text" class="form-control" id="validationServer02" name="secondName">
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+            </div>
 
-    <form action = "" method = "POST">
-      <div class="form-row">
-        <div class="col-md-3 mb-3">
-          <label for="validationServer01">Primer Nombre</label>
-          <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required name = "firstName">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer02">Segundo Nombre (Opcional)</label>
-          <input type="text" class="form-control is-valid" id="validationServer02" value="Otto" required name = "secondName">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer03">Apellido Paterno</label>
-          <input type="text" class="form-control is-valid" id="validationServer03" value="Otto" required name = "firstLastName">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer04">Apellido Materno</label>
-          <input type="text" class="form-control is-valid" id="validationServer04" value="Otto" required name = "secondLastName">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
-      </div>
-      <div class="form-row">
-        <div class="col-md-4 mb-3">
-          <label for="validationServer05">Correo Electronico</label>
-          <input type="text" class="form-control is-valid" id="validationServer05" value="Mark" required name = "email">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
+            <div class="form-row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <label for="validationServer03">Apellido Paterno</label>
+                    <input type="text" class="form-control " id="validationServer03" required name="firstLastName">
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+                <div class="col-sm-4">
+                    <label for="validationServer04">Apellido Materno</label>
+                    <input type="text" class="form-control " id="validationServer04" required name="secondLastName">
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4">
+                    <label for="birthday"> Fecha de Nacimiento</label>
+                    <input type="date" class="form-control" name="birthday" id="birthday" required>
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+                <div class="col-sm-4">
+                    <label for="validationServer10">Estado Civil</label>
+                    <select class="custom-select" id="validationServer10" required name="statusCivil">
+                        <option selected disabled value="">Elige...</option>
+                        <option value="solter(a)">Soltero(a)</option>
+                        <option value="Casado(a)">Casado(a)</option>
+                        <option value="Divorciado(a)">Divorciado(a)</option>
+                        <option value="Viudo(a)">Viudo(a)</option>
+                        <option value="Union Libre">Union Libre</option>
+                    </select>
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+            </div>
 
 
-        <div class="form-group col-md-2">
-          <label for="inputPassword4">Contraseña</label>
-          <input type="password" class="form-control" id="inputPassword4" name = "password">
-        </div>
+            <hr class="solid" style="border-top: 2px solid rgb(134, 8, 8)">
 
 
-        <div class="form-group col-md-2">
-          <label for="inputPassword4">Confirmar Contraseña</label>
-          <input type="password" class="form-control" id="inputPassword4">
-        </div>
+            <div class="form-row">
 
-        <div class="col-md-3 mb-3">
-          <label for="validationServer06">Telefono</label>
-          <input type="text" class="form-control is-valid" id="validationServer06" value="Otto" required name = "phone">
-          <div class="valid-feedback">
-            Bien!
-          </div>
-        </div>
+                
+                <div class="col-sm-5">
+                    <div class="modal-dialog text-center">
+                        <h3>Datos Iglesia</h3>
+                    </div>
+                    <label for="validationServer07">Status de Servicio</label>
+                    <select class="custom-select" id="validationServer07" required name="statusService">
+                        <option selected disabled value="">Elige...</option>
+                        <option value="yes">Si</option>
+                        <option value="no">No</option>
+                    </select>
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                    <label for="validationServer08">Status de Bautizo</label>
+                    <select class="custom-select" id="validationServer08" required name="statusBautizo">
+                        <option selected disabled value="">Elige...</option>
+                        <option value="yes">Si</option>
+                        <option value="no">No</option>
+                    </select>
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                    <label for="validationServer09">Lider Grupo Familiar</label>
+                    <select class="custom-select" id="validationServer09" required name="lider">
+                        <option selected disabled value="">Elige...</option>
+                        <?php
+                              $lideres = new ConsultaController();
+                              $lideres = $lideres->getAllLideres();  
+                              for($i = 0; $i < count($lideres); $i++) {
+                                $name = $lideres[$i]['firstName'] . ' ' . $lideres[$i]['secondName'] . " " . $lideres[$i]['firstLastName'] . " " . $lideres[$i]['secondLastName'];
+                                echo '<option value = ' . $lideres[$i]['id'] . '>' . $name . '</option>';
+                                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                              }
+                            ?>
+                    </select>
+                    <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-5">
+                    <div class="modal-dialog text-center">
+                        <h3>Datos de Contacto</h3>
+                    </div>
+                    <div class="justify-content-center">
+                        <label for="validationServer06">Telefono</label>
+                        <input type="text" class="form-control" id="validationServer06" placeholder="(55) 1234 5678"
+                        required name="phone">
+                        <div class="invalid-feedback">Llena el campo</div>
+                    <div class="valid-feedback">Ok!</div>
+                        <label for="validationServer05">Correo Electronico</label>
+                        <input type="email" class="form-control" id="validationServer05" placeholder="micorreo@ejemplo.com"
+                        required name="email">
+                        <div class="invalid-feedback">Llena el campo</div>
+                        <div class="valid-feedback">Ok!</div>
+                    <label for="password">Contraseña</label>
+                    <input type="password" class="form-control" id="password" name="password" required
+                        minlength="8" maxlength="20">
+                    <div class="invalid-feedback">Debe contener mínimo 8 carácteres</div>
+                    <div class="valid-feedback">Ok!</div>
+                    <label for="password1">Confirmar Contraseña</label>
+                    <input type="password" class="form-control" id="password1" required>    
 
-      </div>
+                    <div class="invalid-feedback">Las contraseñas no Coinciden</div>
+                    <div class="valid-feedback">Ok!</div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+            <br>
+            <div class="form-row">
+                <div class="col-sm-5"></div>
+                <div class="col-sm-3">
+
+                    <button type="submit" class="btn btn-success" data-toggle="modal">
+                        Registrarme
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">¡Buen Trabajo!</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    Tu registro esta siendo procesado. Te enviaremos un correo al email
+                                    proporcionado para terminar tu proceso
+                                    satisfactoriamente
+                                </div>
+                                <div class="modal-footer">
+                                    <a class="btn btn-success" href="#" role="button">Aceptar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
-      <div class="row">
-        <label for="inputState"> Fecha de Nacimiento</label>
-      </div>
-      <div class="form-row">
-        <div class="col-md-4">
-          <input type="text" class="form-control" placeholder="Día (DD)" name = "day">
-        </div>
-        <div class="col-md-4">
-          <input type="text" class="form-control" placeholder="Mes (MM)" name = "month">
-        </div>
-        <div class="col-md-4">
-          <input type="text" class="form-control" placeholder="Año (AAAA)" name = "year">
-        </div>
-      </div>
-
-      <div class="form-row">
-        <div class="col-md-3 mb-3">
-          <label for="validationServer07">Status de Servicio</label>
-          <select class="custom-select is-invalid" id="validationServer07" required name = "statusService">
-            <option selected disabled value="">Elige...</option>
-            <option value = "yes">Si</option>
-            <option value = "no">No</option>
-          </select>
-          <div class="invalid-feedback">
-            Please select a valid state.
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer08">Status de Bautizo</label>
-          <select class="custom-select is-invalid" id="validationServer08" required name = "statusBautizo">
-            <option selected disabled value="">Elige...</option>
-            <option value = "yes">Si</option>
-            <option value = "no">No</option>
-          </select>
-          <div class="invalid-feedback">
-            Please select a valid state.
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer09">Lider Grupo Familiar</label>
-          <select class="custom-select is-invalid" id="validationServer09" required name = "lider">
-            <option selected disabled value="">Elige...</option>
-            <?php
-              $lideres = new ConsultaController();
-              $lideres = $lideres->getAllLideres();  
-              for($i = 0; $i < count($lideres); $i++) {
-                $name = $lideres[$i]['firstName'] . ' ' . $lideres[$i]['secondName'] . " " . $lideres[$i]['firstLastName'] . " " . $lideres[$i]['secondLastName'];
-                echo '<option value = ' . $lideres[$i]['id'] . '>' . $name . '</option>';
-                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              }
-            ?>
-          </select>
-          
-          <div class="invalid-feedback">
-            Please select a valid state.
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="validationServer10">Estado Civil</label>
-          <select class="custom-select is-invalid" id="validationServer10" required name = "statusCivil">
-            <option selected disabled value="">Elige...</option>
-            <option value = "solter(a)">Soltero(a)</option>
-            <option value = "Casado(a)">Casado(a)</option>
-            <option value = "Divorciado(a)">Divorciado(a)</option>
-            <option value = "Viudo(a)">Viudo(a)</option>
-            <option value = "Union Libre">Union Libre</option>
-          </select>
-          <div class="invalid-feedback">
-            Please select a valid state.
-          </div>
-        </div>
-
-      </div>
-     
-      <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col">
-          <button class="btn btn-primary" type="submit">Registrar</button>
-        </div>
-      </div>
-    </form>
-
-    <br>
-    <div class="alert alert-success" role="alert">
-      <h4 class="alert-heading">¡Buen Trabajo!</h4>
-      <p>Tu registro esta siendo procesado. Te enviaremos un correo al email proporcionado para terminar tu proceso
-        satisfactoriamente</p>
-      <hr>
-      <p class="mb-0">Sigue las instrucciones cuidadosamente</p>
+        </form>
     </div>
-  </div>
 
 
-
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
     crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
